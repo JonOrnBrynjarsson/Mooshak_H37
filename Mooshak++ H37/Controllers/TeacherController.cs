8d7ae8330ede33950ad46913ad0a97bf90381404
KalliKall <out1106@gmail.com>
2016-05-05 15:01:00 +0000

    courses service og teacher courseModelview

--- Mooshak++ H37/Controllers/TeacherController.cs
+++ Mooshak++ H37/Controllers/TeacherController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mooshak___H37.Services;
 
 namespace Mooshak___H37.Controllers
 {
     public class TeacherController : Controller
     {
+        CoursesService _courseService = new CoursesService();
+
         // GET: Teacher
         public ActionResult Index()
         {
-            return View();
+            var viewModel = _courseService.getAllCourses();
+            return View(viewModel);
         }
     }
 }
--- Mooshak++ H37/Services/CoursesService.cs
+++ Mooshak++ H37/Services/CoursesService.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Mooshak___H37.Models.Entities;
 using System.Threading.Tasks;
+using Mooshak___H37.Models.Viewmodels;
 
 namespace Mooshak___H37.Services
 {
@@ -16,5 +18,70 @@ namespace Mooshak___H37.Services
 			_db = new ApplicationDbContext();
 		}
 
-	}
+        public List<TeacherCourseViewModel> getAllCourses()
+        {
+
+            var Courses = (from x in _db.Courses
+                           select x).ToList();
+
+            var viewModel = new List<TeacherCourseViewModel>();
+
+            foreach (var course in Courses)
+            {
+                TeacherCourseViewModel model = new TeacherCourseViewModel
+                {
+                    Name = course.Name,
+                    StartDate = course.Startdate,
+                    ID = course.ID,
+                    Isactive = course.Isactive,
+                };
+                viewModel.Add(model);
+            }
+
+            return viewModel;
+        }
+
+        public List<TeacherCourseViewModel> getAllCoursesByID(string UserID)
+        {
+            List<Course> Courses = null;
+
+            var coursesID = (from x in _db.UserCourseRelations
+                             where x.UserID == UserID
+                             select x.CourseID);
+
+            foreach (var id in coursesID)
+            {
+                Courses.Add(getCourseByID(id));
+            }
+
+            var viewModel = new List<TeacherCourseViewModel>();
+
+            foreach (var course in Courses)
+            {
+                TeacherCourseViewModel model = new TeacherCourseViewModel
+                {
+                    Name = course.Name,
+                    StartDate = course.Startdate,
+                    ID = course.ID,
+                    Isactive = course.Isactive,
+                };
+                viewModel.Add(model);
+            }
+
+            return viewModel;
+
+        }
+
+        public Course getCourseByID(int courseID)
+        {
+
+            var course = (from x in _db.Courses
+                         where x.ID == courseID
+                         select x).SingleOrDefault();
+
+            return course;
+        }
+
+
+    }
 }
