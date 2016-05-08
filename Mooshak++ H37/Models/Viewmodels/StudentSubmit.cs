f38cf7fc934405e6d26d2773801077cbb837814b
JonOrnBrynjarsson <jonoganna@hotmail.com>
2016-05-08 16:18:39 +0000

    submit dót

---
+++ Mooshak++ H37/Models/Viewmodels/StudentSubmit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Mooshak___H37.Models.Viewmodels
+{
+	public class StudentSubmit
+	{
+		[ConfigurationPropertyAttribute("maxRequestLength", DefaultValue = 50000)]
+		public int milestone { get; set; }
+		public HttpPostedFile file { get; set; }
+	}
+}
