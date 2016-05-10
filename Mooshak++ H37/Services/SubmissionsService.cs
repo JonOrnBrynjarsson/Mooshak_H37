a820dd4d18a37ed159c12913954a8408d1422576
Barði Freyr Þorsteinsson <Bari Freyr orsteinsson>
2016-05-10 22:13:36 +0000

    Student og Teacher view Submissions

--- Mooshak++ H37/Services/SubmissionsService.cs
+++ Mooshak++ H37/Services/SubmissionsService.cs
@@ -8,148 +8,175 @@ using System.Web;
 
 namespace Mooshak___H37.Services
 {
-	public class SubmissionsService
-	{
-		private ApplicationDbContext _db;
+    public class SubmissionsService
+    {
+        private ApplicationDbContext _db;
 
-		public SubmissionsService()
-		{
-			_db = new ApplicationDbContext();
-		}
+        public SubmissionsService()
+        {
+            _db = new ApplicationDbContext();
+        }
 
-		public int GetCurrentUser()
-		{
-			var currentUser = System.Web.HttpContext.Current.User.Identity.GetUserId();
-			var currUserID = (from x in _db.Users
-							  where x.AspNetUserId == currentUser
-							  select x.ID).FirstOrDefault();
+        public int GetCurrentUser()
+        {
+            var currentUser = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            var currUserID = (from x in _db.Users
+                              where x.AspNetUserId == currentUser
+                              select x.ID).FirstOrDefault();
 
-			return currUserID;
-		}
+            return currUserID;
+        }
 
-		public List<SubmissionsViewModel> GetSubmissionsForMilestone (int milestoneID)
-		{
-			var currUsId = GetCurrentUser();
+        public List<SubmissionsViewModel> GetSubmissionsForMilestone(int milestoneID)
+        {
+            var currUsId = GetCurrentUser();
 
-			var submissions = (from subs in _db.Submissions
-							   where subs.MilestoneID == milestoneID
-							   && subs.IsRemoved != true
-							   select subs).ToList();
+            var submissions = (from subs in _db.Submissions
+                               where subs.MilestoneID == milestoneID
+                               && subs.IsRemoved != true
+                               select subs).ToList();
 
-			var viewModel = new List<SubmissionsViewModel>();
+            var viewModel = new List<SubmissionsViewModel>();
 
-			foreach (var subs in submissions)
-			{
-				SubmissionsViewModel model = new SubmissionsViewModel
-				{
-					Grade = subs.Grade,
-					ID = subs.ID,
-					IsGraded = subs.IsGraded,
-					IsRemoved = subs.IsRemoved,
-					MilestoneID = subs.MilestoneID,
-					Milestone = subs.Milestone,
-					ProgramFileLocation = subs.ProgramFileLocation,
-					UserID = subs.UserID,
-					DateSubmitted = subs.DateSubmitted,
-					UserName = (from name in _db.Users
-								where name.ID == subs.UserID
-								select name.Name).FirstOrDefault()
-			};
-				viewModel.Add(model);
-			}
+            foreach (var subs in submissions)
+            {
+                SubmissionsViewModel model = new SubmissionsViewModel
+                {
+                    Grade = subs.Grade,
+                    ID = subs.ID,
+                    IsGraded = subs.IsGraded,
+                    IsRemoved = subs.IsRemoved,
+                    MilestoneID = subs.MilestoneID,
+                    Milestone = subs.Milestone,
+                    ProgramFileLocation = subs.ProgramFileLocation,
+                    UserID = subs.UserID,
+                    DateSubmitted = subs.DateSubmitted,
+                    UserName = (from name in _db.Users
+                                where name.ID == subs.UserID
+                                select name.Name).FirstOrDefault()
+                };
+                viewModel.Add(model);
+            }
 
-			return viewModel;
-		}
+            return viewModel;
+        }
 
-		public SubmissionsViewModel GetSubmission(int submissionID)
-		{
-			//var currUsId = GetCurrentUser();
+        public SubmissionsViewModel GetSubmission(int submissionID)
+        {
+            //var currUsId = GetCurrentUser();
 
-			var subs = (from subm in _db.Submissions
-							   where subm.ID == submissionID
-							   && subm.IsRemoved != true
-							   select subm).FirstOrDefault();
+            var subs = (from subm in _db.Submissions
+                        where subm.ID == submissionID
+                        && subm.IsRemoved != true
+                        select subm).FirstOrDefault();
 
-			SubmissionsViewModel model = new SubmissionsViewModel
-			{
-				Grade = subs.Grade,
-				ID = subs.ID,
-				IsGraded = subs.IsGraded,
-				IsRemoved = subs.IsRemoved,
-				MilestoneID = subs.MilestoneID,
-				Milestone = subs.Milestone,
-				//ProgramFileLocation = subs.ProgramFileLocation,
-				DateSubmitted = subs.DateSubmitted,
-				UserID = subs.UserID,
-				UserName = (from name in _db.Users
-							where name.ID == subs.UserID
-							select name.Name).FirstOrDefault()
-			};
+            SubmissionsViewModel model = new SubmissionsViewModel
+            {
+                Grade = subs.Grade,
+                ID = subs.ID,
+                IsGraded = subs.IsGraded,
+                IsRemoved = subs.IsRemoved,
+                MilestoneID = subs.MilestoneID,
+                Milestone = subs.Milestone,
+                //ProgramFileLocation = subs.ProgramFileLocation,
+                DateSubmitted = subs.DateSubmitted,
+                UserID = subs.UserID,
+                UserName = (from name in _db.Users
+                            where name.ID == subs.UserID
+                            select name.Name).FirstOrDefault()
+            };
 
-			return model;
-		}
+            return model;
+        }
 
-		public int GetMilestoneIDFromSubmissionID(int submissionID)
-		{
-			var milestone = (from mil in _db.Submissions
-							 where mil.ID == submissionID
-							 select mil.MilestoneID).FirstOrDefault();
+        public int GetMilestoneIDFromSubmissionID(int submissionID)
+        {
+            var milestone = (from mil in _db.Submissions
+                             where mil.ID == submissionID
+                             select mil.MilestoneID).FirstOrDefault();
 
-			return milestone;
-		}
+            return milestone;
+        }
 
-		internal void GradeAssignment(SubmissionsViewModel model)
-		{
-			var currSubmission = (from subs in _db.Submissions
-						where subs.ID == model.ID
-						select subs).FirstOrDefault();
+        internal void GradeAssignment(SubmissionsViewModel model)
+        {
+            var currSubmission = (from subs in _db.Submissions
+                                  where subs.ID == model.ID
+                                  select subs).FirstOrDefault();
 
-			if (currSubmission != null)
-			{
-				currSubmission.Grade = model.Grade;
-				currSubmission.IsGraded = true;
+            if (currSubmission != null)
+            {
+                currSubmission.Grade = model.Grade;
+                currSubmission.IsGraded = true;
 
-				_db.SaveChanges();
-			}
-			else
-			{
-				// DO Something!!
-			}
-		}
+                _db.SaveChanges();
+            }
+            else
+            {
+                // DO Something!!
+            }
+        }
 
-		public List<SubmissionsViewModel> GetSubmissionsForMilestoneForStudent(int milestoneID)
-		{
-			var currUser = GetCurrentUser();
-			var submissions = (from subs in _db.Submissions
-							   where subs.MilestoneID == milestoneID &&
-							   subs.UserID == currUser
-							   select subs).ToList();
+        public List<SubmissionsViewModel> GetSubmissionsForMilestoneForStudent(int milestoneID)
+        {
+            var currUser = GetCurrentUser();
+            var submissions = (from subs in _db.Submissions
+                               where subs.MilestoneID == milestoneID &&
+                               subs.UserID == currUser
+                               select subs).ToList();
 
-			var viewModel = new List<SubmissionsViewModel>();
+            var viewModel = new List<SubmissionsViewModel>();
 
 
-			foreach (var subs in submissions)
-			{
-				SubmissionsViewModel model = new SubmissionsViewModel
-				{
-					Grade = subs.Grade,
-					ID = subs.ID,
-					IsGraded = subs.IsGraded,
-					IsRemoved = subs.IsRemoved,
-					MilestoneID = subs.MilestoneID,
-					Milestone = subs.Milestone,
-					DateSubmitted = subs.DateSubmitted,
-					ProgramFileLocation = subs.ProgramFileLocation,
-					UserID = subs.UserID,
-					UserName = (from name in _db.Users
-								where name.ID == subs.UserID
-								select name.Name).FirstOrDefault()
-				};
-				viewModel.Add(model);
+            foreach (var subs in submissions)
+            {
+                SubmissionsViewModel model = new SubmissionsViewModel
+                {
+                    Grade = subs.Grade,
+                    ID = subs.ID,
+                    IsGraded = subs.IsGraded,
+                    IsRemoved = subs.IsRemoved,
+                    MilestoneID = subs.MilestoneID,
+                    Milestone = subs.Milestone,
+                    DateSubmitted = subs.DateSubmitted,
+                    ProgramFileLocation = subs.ProgramFileLocation,
+                    UserID = subs.UserID,
+                    UserName = (from name in _db.Users
+                                where name.ID == subs.UserID
+                                select name.Name).FirstOrDefault()
+                };
+                viewModel.Add(model);
 
-			}
+            }
 
-			return viewModel;
-		}
-	}
+            return viewModel;
+        }
+
+
+        public SubmissionsViewModel GetOneSubmission(int submissionID)
+        {
+            var submission = (from subs in _db.Submissions
+                              where subs.ID == submissionID
+                              select subs).FirstOrDefault();
+
+            SubmissionsViewModel model = new SubmissionsViewModel
+            {
+                Grade = submission.Grade,
+                ID = submission.ID,
+                IsGraded = submission.IsGraded,
+                IsRemoved = submission.IsRemoved,
+                MilestoneID = submission.MilestoneID,
+                Milestone = submission.Milestone,
+                DateSubmitted = submission.DateSubmitted,
+                ProgramFileLocation = submission.ProgramFileLocation,
+                UserID = submission.UserID,
+                UserName = (from name in _db.Users
+                            where name.ID == submission.UserID
+                            select name.Name).FirstOrDefault()
+            };
+
+            return model;
+
+        }
+    }       
 }
