e61e361347f59560e7af9848ee606f91073a6fc5
JonOrnBrynjarsson <jonoganna@hotmail.com>
2016-05-12 16:56:42 +0000

    admin error

---
+++ Mooshak++ H37/Migrations/201605121627358_errormessage.cs
@@ -0,0 +1,16 @@
+namespace Mooshak___H37.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class errormessage : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
